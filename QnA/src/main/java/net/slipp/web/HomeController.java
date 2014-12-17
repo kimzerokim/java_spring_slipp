346c822f6a0ff46188a524fc24972707d8f15009
YoungKim <ky200223@gmail.com>
2014-12-17 18:07:38 +0900

    logback setting

--- QnA/src/main/java/net/slipp/web/HomeController.java
+++ QnA/src/main/java/net/slipp/web/HomeController.java
@@ -1,13 +1,17 @@
 package net.slipp.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping("/")
 	public String home() {
+		log.debug("template!!");
 		return "home";
 	}
 }
